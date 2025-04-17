31a85464a1cc4335eb94e2cf0d10f0e545efc683
BlakeNeko <blakeneko@outlook.com>
2025-04-17 10:26:15 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style scoped>
+.container {
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  width: 600px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import 'modern-normalize';
+import '@/assets/main.css';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
